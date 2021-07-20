e007338ac0a0e6c09aa6d898af13ed12defa7a12
Alec Peltekian <50842553+alecpeltekian@users.noreply.github.com>
2021-07-20 10:43:07 -0400

    Update contrast.py

--- mmcls/datasets/contrast.py
+++ mmcls/datasets/contrast.py
@@ -54,8 +54,8 @@ class ContrastDataset(BaseDataset):
         data_infos = []
 
         for row in df_split.iterrows():
-            pc_img = row[1][self.CLASSES[0]]
-            syn_img = row[1][self.CLASSES[1]]
+            pc_img = 'PC/' + row[1][self.CLASSES[0]]
+            syn_img = 'SYNTHCON/' + row[1][self.CLASSES[1]]
 
             # create Custom Middle dataset format
             for idx, img in enumerate([pc_img, syn_img]):
