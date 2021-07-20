4b47fe85efbdb596548a13774270236fe8c5b401
Alec Peltekian <50842553+alecpeltekian@users.noreply.github.com>
2021-07-20 13:57:38 +0700

    add numpy contiguous in to_tensor()

--- mmcls/datasets/pipelines/formating.py
+++ mmcls/datasets/pipelines/formating.py
@@ -58,7 +58,7 @@ class ImageToTensor(object):
             img = results[key]
             if len(img.shape) < 3:
                 img = np.expand_dims(img, -1)
-            results[key] = to_tensor(img.transpose(2, 0, 1))
+            results[key] = to_tensor(np.ascontiguousarray(img.transpose(2, 0, 1)))
         return results
 
     def __repr__(self):
