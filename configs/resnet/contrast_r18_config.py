bb98dfe9c4443b0e76a0a2e1c30f936cdd14d8c0
Alec Peltekian <50842553+alecpeltekian@users.noreply.github.com>
2021-07-22 02:00:12 +0700

    Update contrast_r18_config.py

--- configs/resnet/contrast_r18_config.py
+++ configs/resnet/contrast_r18_config.py
@@ -5,14 +5,17 @@
 # dataset settings
 dataset_type = 'ContrastDataset'
 data_root = '/mnt/cadlabnas/datasets/' 
-
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 train_pipeline = [
     dict(type='RandomFlip', flip_prob=0.5, direction='horizontal'),
+    dict(type='Normalize', **img_norm_cfg),
     dict(type='ImageToTensor', keys=['img']),
     dict(type='ToTensor', keys=['gt_label']),
     dict(type='Collect', keys=['img', 'gt_label']),
 ]
 test_pipeline = [
+    dict(type='Normalize', **img_norm_cfg),
     dict(type='ImageToTensor', keys=['img']),
     dict(type='Collect', keys=['img']),
 ]
