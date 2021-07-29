5ae447c08a1555008ef37f10b3cc16a94bbbaa41
Alec Peltekian <50842553+alecpeltekian@users.noreply.github.com>
2021-07-28 22:58:05 -0400

    Update contrast_r18_config.py

--- configs/resnet/contrast_r18_config.py
+++ configs/resnet/contrast_r18_config.py
@@ -12,7 +12,7 @@ train_pipeline = [
     dict(type='RandomFlip', flip_prob=0.5, direction='horizontal'),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='ImageToTensor', keys=['img']),
-    dict(type='Resize', img_scale=(1333, 800), keep_ratio=True)
+    dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
     dict(type='ToTensor', keys=['gt_label']),
     dict(type='Collect', keys=['img', 'gt_label']),
     dict(
