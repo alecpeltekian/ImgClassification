bf617435959fc53b1c3b2607f6081219e4effe17
Alec Peltekian <50842553+alecpeltekian@users.noreply.github.com>
2021-07-31 08:56:53 +0700

    Update prepare_scan_data.py

--- prepare_scan_data.py
+++ prepare_scan_data.py
@@ -3,10 +3,10 @@ import os
 import random
 import cv2
 from scipy import ndimage
-import matplotlib
 import nibabel as nib
 import numpy as np
 import pandas as pd
+import copy
 
 
 def read_nifti_file(filepath):
@@ -57,10 +57,6 @@ def process_scan(path):
     volume = normalize(volume)
     # Resize width, height and depth
     volume = resize_volume(volume)
-    # Convert (width, height, depth) -> (depth, width, height)
-    volume = volume.transpose((2, 0, 1))
-    # Add channels (depth, width, height) -> (depth, width, height, 3)
-    volume = np.repeat(np.expand_dims(volume, axis=-1), 3, axis=-1)
     return volume
 
 
@@ -88,7 +84,54 @@ with open(new_csv_file, 'w') as f:
         n_images = scan.shape[0]
         row_data[folder + '_num'] = n_images
         row_data[folder] = filename
-        for i in range(n_images):
-          cv2.imwrite(f'{path_dir}/{folder}/img/{filename[:filename.find(".")]}_{i}.png', scan[i])
 
+        # for a nifti file, dim_0 is image width, dim_1 is height, dim_2 is channels/slices
+        im_width, im_height, nifti_channels = np.asarray(scan).shape
+        im_channels = 3
+        half_c = im_channels // 3
+        for i in range(n_images):
+          # check bounds of data
+          # within
+          if (i - half_c >= 0) & (i + half_c <= nifti_channels - half_c):
+              slice_start = i - half_c
+              slice_end = i + half_c + 1
+              # get temporary sub_slice
+              t_sub_slice = scan[:,:,slice_start:slice_end]
+
+          # outside left edge
+          elif i - half_c < 0:
+              # get temporary sub_slice
+              t_sub_slice = scan[:,:,0:im_channels]
+
+          # outside right edge
+          elif i + half_c > nifti_channels - 1:
+              # get temporary sub_slice
+              t_sub_slice = scan[:,:,nifti_channels-im_channels:nifti_channels]
+
+
+          # current shape of subslice is [width, height, channels]
+          # need to change shape to [height, width, channels] for saving to disk/training
+          curr_sub_slice = np.zeros([im_height, im_width, im_channels], dtype = np.uint8)
+
+          # histogram equalization (adaptive with CLAHE)
+          # run histEq on the normalized_9x_percentile midSlice
+          ksize = 8
+          clahe = cv2.createCLAHE(clipLimit=1.0, tileGridSize=(ksize, ksize))
+          for channel_idx in range(im_channels):
+              # transpose individual slice  ----- [width, height] --> [height, width]
+              curr_slice = np.transpose(t_sub_slice[:,:,channel_idx])
+
+              # normalize the slice
+              # norm_9x_img = min_max_normalization(curr_slice, rquantile = 1)
+              histEq_img = clahe.apply(np.asarray(curr_slice).astype(np.uint8)) 
+
+              # place in position
+              curr_sub_slice[:,:,channel_idx] = copy.deepcopy(histEq_img)
+
+          # opencv saves images in BGR format (not RGB -- so interchange first and last images in sub-slice)
+          tempRed = copy.deepcopy(curr_sub_slice[:,:,0])
+          curr_sub_slice[:,:,0] = copy.deepcopy(curr_sub_slice[:,:,2])
+          curr_sub_slice[:,:,2] = copy.deepcopy(tempRed)
+          cv2.imwrite(f'{path_dir}/{folder}/img/{filename[:filename.find(".")]}_{i}.png', curr_sub_slice)
+  
     writer.writerow(row_data)
